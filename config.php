0d1ed0958068dd49fd2b86de61c67c9667087d73
Anthony Swanson <anthonyswanson@accentinc.org>
2011-03-29 07:11:50 -0400

    Messed up last, adding file directories, etc.

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// CONFIG.PHP
+
+// Below are the customizable variables for the script, you may edit 
+// them until you reach the part that says, "STOP HERE." 
+
+
+
+// BEGIN CONFIGURATION HERE
+
+// Use flat file system or MySQL Database (0 - MySQL, 1 - Flat File)
+$mg_backend = "";
+
+?>
