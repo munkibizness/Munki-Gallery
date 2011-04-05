780c7b7352caa1dc0fb243c9eb8a4d486e60ca62
Anthony Swanson <anthonyswanson@accentinc.org>
2011-04-05 16:42:48 -0400

    Add MySQL settings to config.php

--- config.php
+++ config.php
@@ -11,4 +11,22 @@
 // Use flat file system or MySQL Database (0 - MySQL, 1 - Flat File)
 $mg_backend = "";
 
+// If you opted to use the Flat File system STOP CONFIGURATION HERE
+// otherwise, skip the next line and continue editing.
+if ($mg_backend == "1") die();
+
+// Begin MySQL Database Information
+// MySQL Host Name 
+$db_host = "localhost"; 
+
+// MySQL Database Name
+$db_name = "";
+
+// MySQL User Name 
+$db_username = "";
+
+// MySQL User Pass 
+$db_username = "";
+
+// End MySQL Database Information
 ?>
